94533d9ff4e892c4a03a5a5cb26bdb560d42bd81
Brayteh <ya3koub5@gmail.com>
2025-06-20 11:08:39 +0200

    Aufg_2

---
+++ String.dart
@@ -0,0 +1,15 @@
+String fullName (String firstName, String lastName) {
+    return "${firstName} ${lastName}";
+
+}
+
+void main () {
+
+    String user1 = ("Tom, Jonson");
+    String user2 = ("Alex, Müller");
+    String user3 = ("Ali, Hassan");
+
+    print("The first user is: $user1");
+    print("the second user is: $user2");
+    print("the third user is: $user3");
+}
