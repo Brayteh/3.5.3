57e2d6e748ca43c3668625eefe8f55afb7b64035
Brayteh <ya3koub5@gmail.com>
2025-06-20 10:56:22 +0200

    Aufg_1

---
+++ main.dart
@@ -0,0 +1,13 @@
+int sum (int a, int b, int c) {
+    return a + b + c;
+}
+
+void main() {
+    int result1 = sum(10, 20, 30);
+    print ("The sum 1 is: $result1");
+    int result2 = sum(39, 23, 45);
+    print("The sum 2 is: $result2");
+    int result3 = sum(100,200,300);
+    print("The sum 3 is: $result3");
+
+}
